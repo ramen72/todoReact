33883411e93b8581e864246fedf379e105e52daf
ramen72 <ramen@devsramen.com>
2025-04-12 12:35:04 +0600

    Update AddTodo, deleteTodo, editTodo at 12-04-2025_1235

--- src/components/home/HomeComponent.jsx
+++ src/components/home/HomeComponent.jsx
@@ -1,17 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import { FaPenToSquare } from "react-icons/fa6";
 import { GiCheckMark } from "react-icons/gi";
 import { FaRegTrashCan } from "react-icons/fa6";
-import { addTodo, deleteTodo } from '../../slices/todo/todoSlice';
+import { addTodo, completeTodo, deleteTodo, editTodo, inputValue, updateTodo } from '../../slices/todo/todoSlice';
 
 const HomeComponent = () => {
-
-    let [todoText,setTodoText] = useState("")
     
     const data = useSelector((state) => state.todo.value)
+    const inValue = useSelector((state) => state.todo.inputValue)
+    const addBtn = useSelector((state) => state.todo.addBtn)
+    const updateIndex = useSelector((state) => state.todo.updateIndex)
+    
+
+
     const dispatch = useDispatch()
-    console.log(data);
     
 
     return (
@@ -20,9 +23,12 @@ const HomeComponent = () => {
                 <main id="main" className="main">
                     <h1 className="heading text-center text-white text-4xl font-bold mb-4">ToDo List</h1>
                     <div className="form-control bg-white rounded-md py-2 pl-2 mb-4">
-                        <input onChange={(e)=>setTodoText(e.target.value)} type="text" name="item" id="newItemText" className='border border-[#040b3cdd]' placeholder="Write you task"/>
-                        <button onClick={()=>dispatch(addTodo(todoText))} id="addNewItemBtn" type="submit">Add</button>
-                        <button id="updateItemBtn" type="submit">Update</button>
+                        {/* <input onChange={(e)=>setTodoText(e.target.value)} type="text" name="item" id="newItemText" className='border border-[#040b3cdd]' placeholder="Write you task"/> */}
+                        <input onChange={(e)=>dispatch(inputValue(e.target.value))} value={inValue} type="text" name="item" id="newItemText" className='border border-[#040b3cdd]' placeholder="Write you task"/>
+                        {
+                            addBtn ? <button onClick={()=>dispatch(addTodo(inValue))} id="addNewItemBtn" type="submit">Add</button> : <button onClick={()=>dispatch(updateTodo())} id="updateItemBtn" type="submit">Update</button>
+                        }
+
                     </div>
                     <div className="content">
                         <div className="pending">
@@ -32,11 +38,11 @@ const HomeComponent = () => {
                                     data.map((todo,index)=>(
                                         <li className="statusPending" key={index}>
                                             <span className="taskItem">{todo}</span>
-                                            <div className="buttonsWrapper">
-                                                <button className="editBtn">
+                                            <div  className="buttonsWrapper">
+                                                <button onClick={()=>dispatch(editTodo({id:index,newValue:todo}))} className="editBtn">
                                                     <FaPenToSquare/>
                                                 </button>
-                                                <button className="doneBtn">
+                                                <button onClick={()=>dispatch(completeTodo())} className="doneBtn">
                                                     <GiCheckMark />
                                                 </button>
                                                 <button onClick={()=>dispatch(deleteTodo(index))} className="pendingDeleteBtn">
--- src/slices/todo/todoSlice.js
+++ src/slices/todo/todoSlice.js
@@ -1,22 +1,47 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-  value: ["ramen","Biswas"],
+  value: [],
+  inputValue: "",
+  addBtn: true,
+  updateIndex: "",
+
+  completeValue:[]
 }
 
 export const todoSlice = createSlice({
   name: 'todo',
   initialState,
   reducers: {
+    inputValue: (state, action) => {
+      state.inputValue = action.payload;
+    },
     addTodo: (state, action) => {
       state.value.push(action.payload)
+      state.inputValue = ""
+      console.log(state)
     },
     deleteTodo: (state, action) => {
       state.value.splice(action.payload, 1)      
     },
+    editTodo: (state, action) => {
+      state.addBtn = false
+      state.inputValue = action.payload.newValue;
+      state.updateIndex = action.payload.id;
+    },
+    updateTodo: (state, action) => {
+      state.addBtn = true
+      state.value[state.updateIndex] = state.inputValue;
+      state.inputValue = ""
+    },
+    completeTodo: (state, action) => {
+      state.value.splice(action.payload, 1)      
+      state.completeValue.push(state.value[action.payload]) 
+      console.log(state.completeValue)     
+    },
   },
 })
 
-export const { addTodo, deleteTodo } = todoSlice.actions
+export const { inputValue, addTodo, deleteTodo, editTodo, updateTodo, completeTodo} = todoSlice.actions
 
 export default todoSlice.reducer
